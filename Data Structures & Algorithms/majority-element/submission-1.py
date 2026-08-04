8b426658d511a25b7d831d6366d9ca9498a1881f
Dhruv Bhadauriya <62044304+ThinkerDesigns@users.noreply.github.com>
2026-08-04 02:03:05 -0700

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.py
@@ -0,0 +1,6 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        freq = {}
+        for x in nums:
+            freq[x] = freq.get(x,0) + 1
+        return max(freq, key=freq.get)
